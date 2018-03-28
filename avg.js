80d1e82246211f3b7a1ed19fc68d53dac6919baa
Zuhair Taha <admin@tahasoft.com>
2018-03-29 01:00:44 +0200

    إصلاح مشكلة بحساب ساعات المواد المعدلة

--- avg.js
+++ avg.js
@@ -2,13 +2,13 @@
 // @name         سكربت حساب المعدل الجامعي
 // @namespace    avgCalc
 // @encoding     utf-8
-// @version      26.3.2018
+// @version      20180329
 // @description  لحساب معدل طلاب الهندسة المعلوماتية في الجامعة الافتراضية السورية
 // @author       زهير طه
 // @include      /(.+)svuonline\.org(.+)/
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @require      http://svu-ise.com/files/js/avg7.js
-// @icon 	 svu-ise.com/files/img/avg.ico
+// @require      http://svu-ise.com/files/js/avg8.js
+// @icon 	     http://svu-ise.com/files/img/avg.ico
 // @homepage 	 http://svu-ise.com/avg
 // @updateURL    http://svu-ise.com/files/js/avg_update.js
 // @downloadURL  https://raw.githubusercontent.com/zuhairtaha/avgCalc/master/avg.js
